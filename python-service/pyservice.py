ffd66431712030cc0aa494d477a9ffc41f6b3387
Guilherme Fontebasso <guilhermenishifonte@gmail.com>
2019-10-06 09:04:09 -0300

    remove gunicorn, add gevent, celery and socketio

--- python-service/pyservice.py
+++ python-service/pyservice.py
@@ -1,6 +1,19 @@
+import os
 from flask import Flask
+from celery import Celery
+from flask_socketio import SocketIO
+from flask_celery import make_celery
+
 app = Flask(__name__)
 
+app.config.update(
+CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379'),
+CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379') 
+)
+
+celery = make_celery(app)
+
+socketio = SocketIO(app, message_queue='redis://redis:6379/0')
 
 @app.route("/")
 def hello():
@@ -9,4 +22,5 @@ def hello():
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, threaded=True)
+    socketio.run(app, host='0.0.0.0', port=5000)
+
--- python-service/wsgi.py
+++
@@ -1,5 +0,0 @@
-from pyservice import app
-
-if __name__ == "__main__":
-    app.run()
-
